8c3b53525bbebd5e94331a5a0fb111f3a9583d3f
Anil Patil <anilrspatil1992@gmail.com>
2023-09-20 23:40:38 +0530

    add controller to manulaeploymen project

---
+++ src/main/java/com/nil/contoller/UserController.java
@@ -0,0 +1,16 @@
+package com.nil.contoller;
+
+
+public class UserController {
+	
+	public static void main(String[] args) {
+		getUserResponse();
+	}
+	
+	public static String getUserResponse() {
+		
+		return "User is available";
+	}
+
+}
+
